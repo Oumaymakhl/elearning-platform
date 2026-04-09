2d3869ae1dfa3ae66ab7bee65a926d28f185ceec
Oumayma <oumaymakahouli19@gmail.com>
2026-04-10 00:54:30 +0200

    Nettoyage des fichiers de backup

--- _backup_ai_quiz_20260409_133423/chatbot_routes_api.php
+++
@@ -1,6 +0,0 @@
-<?php
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ChatController;
-
-Route::get('/ping', [ChatController::class, 'ping']);
-Route::post('/chat', [ChatController::class, 'chat']);
